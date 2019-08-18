d239e3784f26ac971e677ebf6f10f058bf127816
oyxh <13975812120@139.com>
2019-08-18 20:37:46 +0800

    反对反对 Signed-off-by: oyxh <13975812120@139.com>

--- src/utils/EditOverlay.js
+++ src/utils/EditOverlay.js
@@ -12,57 +12,29 @@ EditOverlay.prototype.initialize = function (map, overlay) {
   this._circles = [] // 存储编辑点
   this._middles = [] // 存储中间编辑点(覆盖物)
   let radius = this.getCircleRadius()
-  var that = this
-  var deletePoint = this._deletePoint = function (e, ee, overlay) { // 编辑点右键响应函数
-    console.log('delete Point')
-    map.removeOverlay(overlay)
-    console.log(overlay)
-    console.log(that.getPointIndex(overlay.getCenter()))
-    let index = that.getPointIndex(overlay.getCenter())
-    that._points.splice(index, 1)
-    that._circles.splice(index, 1)
-    console.log(that._polygonData)
-    that._polygonData.splice(index, 1)
-    that.redrawPolygon()
-  }
-  var dragStart = this._dragStart = function (e) {
-    console.log('it dragging')
-  }
   for (let i = 0; i < this._points.length; i++) { // 生成编辑点
     var anewcircle = new window.BMap.Circle(this._points[i], radius)
     anewcircle.setFillColor('blue')
     anewcircle.setFillOpacity(1)
     var markerMenu = new window.BMap.ContextMenu()
-    markerMenu.addItem(new window.BMap.MenuItem('删除点', deletePoint))
+    markerMenu.addItem(new window.BMap.MenuItem('删除点', this.deletePoint.bind(this)))
     anewcircle.addContextMenu(markerMenu) // 以上三行添加编辑点右键菜单
-    anewcircle.addEventListener('click', dragStart)
+    anewcircle.addEventListener('click', this.dragStart.bind(anewcircle))
     this._circles.push(anewcircle)
     map.addOverlay(anewcircle)
     // 以上为多边形的顶点，以下为多边形的线段中点
-    let midlng = (this._points[i].lng + this._points[(i + 1) % this._points.length].lng) / 2
-    let midlat = (this._points[i].lat + this._points[(i + 1) % this._points.length].lat) / 2
-    var addPoints = []
-    addPoints.push(new window.BMap.Point(midlng, midlat))
+    let addPoints = []
+    addPoints.push(this.getMidPoint(this._points[i], this._points[(i + 1) % this._points.length]))
     this.addMiddle(i, 0, addPoints)
-  /*  var middlePoint = new window.BMap.Point(midlng, midlat)
-    this._middlePoints.push(middlePoint)
-    var amiddle = new window.BMap.Circle(middlePoint, radius * 0.9)
-    amiddle.setFillColor('green')
-    amiddle.setFillOpacity(1)
-    this._middles.push(amiddle)
-    map.addOverlay(amiddle) */
   }
-  console.log(this._middles)
-  this._zoomListen = function (e) {
-    console.log('zoomend')
-    let radius = that.getCircleRadius()
-    for (let i = 0; i < that._circles.length; i++) {
-      that._circles[i].setRadius(radius)
-      that._middles[i].setRadius(radius * 0.9)
-    }
-  }
-  this._map.addEventListener('zoomend', this._zoomListen)
-  console.log(this._points)
+  this._map.addEventListener('click', this.stopBubble)
+  this._map.addEventListener('zoomend', this.zoomListen.bind(this))
+  //  this._map.addEventListener('mousemove', this._moveAction)
+}
+EditOverlay.prototype.getMidPoint = function (point1, point2) {
+  let midlng = (point1.lng + point2.lng) / 2
+  let midlat = (point1.lat + point2.lat) / 2
+  return new window.BMap.Point(midlng, midlat)
 }
 EditOverlay.prototype.getCircleRadius = function () {
   var centre = this._map.getCenter()
@@ -72,34 +44,38 @@ EditOverlay.prototype.getCircleRadius = function () {
   return this._map.getDistance(centre, point)
 }
 EditOverlay.prototype.addOverlays = function () {
-  this._map.addEventListener('zoomend', this._zoomListen)
+  this._map.addEventListener('zoomend', this.zoomListen)
+  this._map.addEventListener('click', this.stopBubble)
   for (let i = 0; i < this._circles.length; i++) {
     this._map.addOverlay(this._circles[i])
     this._map.addOverlay(this._middles[i])
   }
 }
 EditOverlay.prototype.remove = function () {
-  this._map.removeEventListener('zoomend', this._zoomListen)
+  this._map.removeEventListener('zoomend', this.zoomListen)
+  this._map.removeEventListener('click', this.stopBubble)
   for (let i = 0; i < this._circles.length; i++) {
     this._map.removeOverlay(this._circles[i])
     this._map.removeOverlay(this._middles[i])
   }
 }
 EditOverlay.prototype.addMiddle = function (startIndex, num, addArray) { // startIndex ，替换起始序号，num替换数量，addArray:增加的中间点的点坐标（Point类型）
-  for (let i = startIndex; i < num; i++) {
-    this._map.removeOverlay(this._middles[i])
+  var arrayLen = this._points.length
+  for (let i = startIndex; i < startIndex + num; i++) {
+    this._map.removeOverlay(this._middles[i % arrayLen])
   }
-  var addMiddlePoint = []
   let radius = this.getCircleRadius()
+  this._middlePoints.splice(startIndex, num)
+  this._middles.splice(startIndex, num)
+  console.log(addArray.length)
   for (let i = 0; i < addArray.length; i++) {
     var amiddle = new window.BMap.Circle(addArray[i], radius * 0.9)
     amiddle.setFillColor('green')
     amiddle.setFillOpacity(1)
-    addMiddlePoint.push(amiddle)
+    this._middlePoints.splice((startIndex + i) % arrayLen, 0, addArray[i])
+    this._middles.splice((startIndex + i) % arrayLen, 0, amiddle)
     this._map.addOverlay(amiddle)
   }
-  this._middlePoints.splice(startIndex, num, addArray)
-  this._middles.splice(startIndex, num, addMiddlePoint)
 }
 EditOverlay.prototype.getPointIndex = function (point) {
   for (let i = 0; i < this._points.length; i++) {
@@ -108,6 +84,54 @@ EditOverlay.prototype.getPointIndex = function (point) {
     }
   }
   return -1
+}
+EditOverlay.prototype.deletePoint = function (e, ee, overlay) { // 编辑点右键响应函数
+  this._map.removeOverlay(overlay)
+  let index = this.getPointIndex(overlay.getCenter())
+  console.log(index)
+  var addPoints = []
+  addPoints.push(this.getMidPoint(this._points[(index + this._points.length - 1) % this._points.length], this._points[(index + 1) % this._points.length]))
+  this.addMiddle((index + this._points.length - 1) % this._points.length, 2, addPoints)
+  this._points.splice(index, 1)
+  this._circles.splice(index, 1)
+  console.log(this._polygonData)
+  this._polygonData.splice(index, 1)
+  this.redrawPolygon()
+}
+EditOverlay.prototype.stopBubble = function (e) {
+  console.log('it _stopBubble')
+  this._map.disableDoubleClickZoom()
+}
+EditOverlay.prototype.moveAction = function (e, index) {
+  console.log('it movine')
+  console.log(e.point.lng + ' ' + e.point.lat)
+  console.log(e.point)
+  console.log(this._circles[index])
+  // that._circles[index].setCenter(e.point)
+  console.log(e.target)
+}
+EditOverlay.prototype.dblclickAction = function (e) {
+  console.log('it dblclickAction')
+  // that._map.removeEventListener('mousedown', dragStart)
+  this._map.removeEventListener('mousemove', this.moveAction)
+  this._map.removeEventListener('dblclick', this.dblclickAction)
+}
+EditOverlay.prototype.dragStart = function (e) {
+  console.log('it dragging')
+  console.log(this)
+  // console.log(this)
+  let index = this.getPointIndex(this.getCenter())
+  this._map.addEventListener('mousemove', this.moveAction)
+  // that._map.addEventListener('mousemove', moveAction(e, index))
+  this._map.addEventListener('dblclick', this.dblclickAction)
+  console.log(index)
+}
+EditOverlay.prototype.zoomListen = function (e) {
+  let radius = this.getCircleRadius()
+  for (let i = 0; i < this._circles.length; i++) {
+    this._circles[i].setRadius(radius)
+    this._middles[i].setRadius(radius * 0.9)
+  }
 }
 EditOverlay.prototype.redrawPolygon = function () {
   this._overlay.setPath(this._points)
